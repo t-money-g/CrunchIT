cf8038dc10ea8a4c9d51838516ed98b3fc123f53
Terrence Young <tyoung@solstice-consulting.com>
2016-09-24 13:18:59 -0500

    Main_Menu.unity and some changes in the main scene including selecting a food item

---
+++ Assets/Board.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Board : MonoBehaviour {
+
+    //List<Tiles> Tiles = [];
+    //List<Tiles> SelectedTiles = [];
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Tile.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tile : MonoBehaviour
+{
+    private bool m_Selected = false;
+    private SpriteRenderer m_spriteRenderer;
+    
+	// Use this for initialization
+	void Start ()
+	{
+	    m_spriteRenderer = GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void OnMouseDown()
+    {
+        m_Selected = true;
+        m_spriteRenderer.color = Color.Lerp(new Color(m_spriteRenderer.color.r, m_spriteRenderer.color.g, m_spriteRenderer.color.b,
+                m_spriteRenderer.color.a), new Color(255, 229, 229), Time.fixedDeltaTime);
+        Debug.Log("Testing 1, 2 ,3");
+        
+        
+    }
+
+    
+}
